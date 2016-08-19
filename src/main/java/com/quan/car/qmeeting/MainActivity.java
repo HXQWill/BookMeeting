5cdc3d7495f408950fc61d18a4b37bdc8c9f98a5
HXQWill <663512441@qq.com>
2016-08-19 11:36:12 +0800

    进展-登录功能实现： 登录功能非空校验，数据库查询，页面跳转的实现。

--- src/main/java/com/quan/car/qmeeting/MainActivity.java
+++ src/main/java/com/quan/car/qmeeting/MainActivity.java
@@ -1,8 +1,10 @@
 package com.quan.car.qmeeting;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -29,7 +31,6 @@ public class MainActivity extends Activity {
         initView();
 
         dbHelper = new MyDatabaseHelper(this, "Meeting.db", null, 1);
-        dbHelper.getWritableDatabase();
 
         login_btn_main.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,15 +39,44 @@ public class MainActivity extends Activity {
                 if(TextUtils.isEmpty(userName)){
                     Toast.makeText(getApplication(), "用户名不能为空！", Toast.LENGTH_SHORT).show();
                 }
-                Log.d("权兴权意-userName:",userName);
                 String passWord = passWord_et_main.getText().toString();
                 if(TextUtils.isEmpty(passWord)){
                     Toast.makeText(getApplication(), "密码不能为空！", Toast.LENGTH_SHORT).show();
                 }
-                Log.d("权兴权意-passWord:",passWord);
-                Log.d("encodeFromC(passWord):",JniUtils.encodeFromC(passWord,passWord.length()));
-                String encodePassWord = JniUtils.encodeFromC(passWord,passWord.length());
-                Log.d("decodeFromC(passWord):",JniUtils.decodeFromC(encodePassWord,encodePassWord.length()));
+
+                if(!TextUtils.isEmpty(userName)&&!TextUtils.isEmpty(passWord)){
+                    Log.d("权兴权意-userName:",userName);
+                    Log.d("权兴权意-passWord:",passWord);
+                    Log.d("encodeFromC(passWord):",JniUtils.encodeFromC(passWord,passWord.length()));
+                    String encodePassWord = JniUtils.encodeFromC(passWord,passWord.length());
+                    Log.d("decodeFromC(passWord):",JniUtils.decodeFromC(encodePassWord,encodePassWord.length()));
+
+                    SQLiteDatabase db = dbHelper.getWritableDatabase();
+                    Cursor cursor = db.query("User", null, null, null, null, null,
+                            null);
+                    if (cursor.moveToFirst()) {
+                        do {
+                            String nameCur = cursor.getString(cursor
+                                    .getColumnIndex("name"));
+                            String passwordCur = cursor.getString(cursor
+                                    .getColumnIndex("password"));
+                            int idCur = cursor.getInt(cursor
+                                    .getColumnIndex("id"));
+                            Log.d("权兴权意：", "nameCur-" + nameCur);
+                            Log.d("权兴权意：", "passwordCur-" + passwordCur);
+                            Log.d("权兴权意：", "idCur-" + idCur);
+
+                            if(userName.equals(nameCur)&&passWord.equals(passwordCur)){
+                                Intent intent = new Intent();
+                                intent.setClass(MainActivity.this, ListActivity.class);
+                                startActivity(intent);
+                                break;
+                            }
+                        } while (cursor.moveToNext());
+                    }
+                    cursor.close();
+                }
+
             }
         });
 
